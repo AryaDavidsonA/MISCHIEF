630710274ad80cf1302379cc6ee9b05e54218a83
AryaDavidsonA <72879382+AryaDavidsonA@users.noreply.github.com>
2020-10-14 21:25:19 +0530

    Add files via upload

---
+++ stegano.py
@@ -0,0 +1,23 @@
+import Decode
+import Encode
+
+
+def stego():
+    print("Hey,It's me parrot!!!")
+    Choice = int(input("Please choose your option!!! \n 1. Encrypt \n 2. Decrypt \n Enter your choice here : "))
+    if Choice == 1:
+        first = input("Enter the first image : ")
+        second = input("Enter the hidden image : ")
+        Encode.encode(first, second)
+        print("Given image encrypted successfully and the image saved as hidden.png!!!")
+
+    elif Choice == 2:
+        dec = input("Enter the image for decode : ")
+        Decode.decode()
+        print("Your image decrypted successfully, the image encrypted from ", dec, " and saved as saved.png!!!")
+    else:
+        raise ValueError("Input valid data")
+
+
+stego()
+
