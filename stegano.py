c0b6c79426c6ae7b6301508485cf5c5fc8e34ed7
AryaDavidsonA <72879382+AryaDavidsonA@users.noreply.github.com>
2020-10-14 20:40:14 +0000

    Update stegano.py

--- stegano.py
+++ stegano.py
@@ -6,15 +6,15 @@ def stego():
     
     Choice = int(input("Please choose your option!!! \n 1. Encrypt \n 2. Decrypt \n Enter your choice here : "))
     if Choice == 1:
-        first = input("Enter the first image : ")
-        second = input("Enter the hidden image : ")
+        first = input("Enter the first image file : ")
+        second = input("Enter the image file for hide : ")
         Encode.encode(first, second)
-        print("Given image encrypted successfully and the image saved as hidden.png!!!")
+        print("Given image encoded successfully and the image saved as hidden.png!!!")
 
     elif Choice == 2:
         dec = input("Enter the image for decode : ")
         Decode.decode()
-        print("Your image decrypted successfully, the image encrypted from ", dec, " and saved as saved.png!!!")
+        print("Your image decoded successfully from ", dec," and saved as saved.png!!!")
     else:
         raise ValueError("Input valid data")
 
