2432f1c1e9bd43ae45412d9ad3ace3dc7f69f3b1
AryaDavidsonA <72879382+AryaDavidsonA@users.noreply.github.com>
2020-10-17 08:59:40 +0000

    Update stegano.py

--- stegano.py
+++ stegano.py
@@ -2,22 +2,26 @@ import Decode
 import Encode
 
 
-def stego():
-    
-    Choice = int(input("Please choose your option!!! \n 1. Encrypt \n 2. Decrypt \n Enter your choice here : "))
+def stegano():
+    Choice = int(input("Please choose your option!!! \n 1. Encode \n 2. Decode \n Enter your choice here : "))
     if Choice == 1:
-        first = input("Enter the first image file : ")
-        second = input("Enter the image file for hide : ")
-        Encode.encode(first, second)
-        print("Given image encoded successfully and saved as hidden.png!!!")
+        first = input("Enter your first image : ")
+        second = input("Enter your second image : ")
+        third = input("Enter your third image : ")
+        Encode.encode(first, second, third)
+        print("Given image encrypted successfully!!!")
+        print("Image saved as hidden.png and secret.png!!!")
 
     elif Choice == 2:
-        dec = input("Enter the image for decode : ")
+
+        dec = input("Enter your image for decode : ")
+        dec_1 = input("Enter your image for decode : ")
         Decode.decode()
-        print("Your image decoded successfully from ", dec," and saved as saved.png!!!")
+        print("Your image decrypted successfully!!!")
+        print("Image decrypted from ", dec, "and", dec_1, "!!!")
+        print("Both image saved as stego.png and stego1.png!!!")
     else:
-        raise ValueError("Input valid data")
+        raise Exception("Choose the correct option!!!")
 
 
-stego()
-
+stegano()
